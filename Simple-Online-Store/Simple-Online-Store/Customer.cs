7a8c92a6cce89ccd32cf2be8be2e0fb7194c78d5
Nikolay <nikolay.zlatev2005@gmail.com>
2023-04-23 12:20:32 +0300

    move the premium cutomer to another class for more readable code

--- Simple-Online-Store/Simple-Online-Store/Customer.cs
+++ Simple-Online-Store/Simple-Online-Store/Customer.cs
@@ -30,22 +30,4 @@ namespace Online_Store
             return cost;
         }
     }
-
-    // this is an example of inheritance
-    // the premium customer have specific property for discount
-    class PremiumCustomer : Customer
-    {
-        public PremiumCustomer(string name, int age, decimal money, int discountPercentage) : base(name, age, money)
-        {
-            DiscountPercentage = discountPercentage;
-        }
-
-        public int DiscountPercentage { get; set; }
-
-        public override decimal CostOfShoppingCard()
-        {
-            decimal cost = ShoppingCard.Items.Sum(x => x.Price) - ShoppingCard.Items.Sum(x => x.Price) * DiscountPercentage / 100;
-            return cost;
-        }
-    }
 }
---
+++ Simple-Online-Store/Simple-Online-Store/PremiumCustomer.cs
@@ -0,0 +1,26 @@
+﻿using Online_Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simple_Online_Store
+{
+    // this is an example of inheritance
+    // the benefit of premium customer is that specific property for discount
+    class PremiumCustomer : Customer
+    {
+        public PremiumCustomer(string name, int age, decimal money, int discountPercentage) : base(name, age, money)
+        {
+            DiscountPercentage = discountPercentage;
+        }
+
+        public int DiscountPercentage { get; set; }
+
+        public override decimal CostOfShoppingCard()
+        {
+            decimal cost = ShoppingCard.Items.Sum(x => x.Price) - ShoppingCard.Items.Sum(x => x.Price) * DiscountPercentage / 100;
+            return cost;
+        }
+    }
+}
