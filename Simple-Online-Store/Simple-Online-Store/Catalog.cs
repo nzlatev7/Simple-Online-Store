e1fee818456fa04e8048188925c724c0e559473f
Nikolay <nikolay.zlatev2005@gmail.com>
2023-04-23 12:21:58 +0300

    making the Checkout methods and Catalog Dictionary static

--- Simple-Online-Store/Simple-Online-Store/Catalog.cs
+++ Simple-Online-Store/Simple-Online-Store/Catalog.cs
@@ -6,24 +6,21 @@ namespace Online_Store
 {
     class Catalog
     {
-        public Catalog()
+        public static Dictionary<int, Item> Items = new Dictionary<int, Item>()
         {
             //creating a dictionary using collection-initializer syntax
-            Items = new Dictionary<int, Item>()
-            {
-                {1,  new Item("pijama", "1m i 20cm" ,12.50m, ItemCategory.Clothing)},
-                {2,  new Item("mouse", "the mouse has LED ligths" ,17.50m, ItemCategory.Electronics)},
-                {3,  new Item("obuwki", "41 nomer" ,10.50m, ItemCategory.Clothing)},
-                {4,  new Item("kolan", "1m" ,5.50m, ItemCategory.Clothing)},
-                {5,  new Item("maratonki", "45 nomer" ,102, ItemCategory.Clothing)},
-                {6,  new Item("teniska", "L" ,17.80m, ItemCategory.Clothing)},
-                {7,  new Item("riza", "41 nomer" ,11.10m, ItemCategory.Clothing)},
-                {8,  new Item("book", "100pages" ,5.90m, ItemCategory.Books)},
-            };
-        }
-        public Dictionary<int, Item> Items { get; set; }
+            { 1,  new Item("pijama", "1m i 20cm", 12.50m, ItemCategory.Clothing) },
+            { 2,  new Item("mouse", "the mouse has LED ligths", 17.50m, ItemCategory.Electronics)},
+            { 3,  new Item("obuwki", "41 nomer", 10.50m, ItemCategory.Clothing)},
+            { 4,  new Item("kolan", "1m", 5.50m, ItemCategory.Clothing)},
+            { 5,  new Item("maratonki", "45 nomer", 102, ItemCategory.Clothing) },
+            { 6,  new Item("teniska", "L", 17.80m, ItemCategory.Clothing)},
+            { 7,  new Item("riza", "41 nomer", 11.10m, ItemCategory.Clothing)},
+            { 8,  new Item("book", "100pages", 5.90m, ItemCategory.Books) }
+        };
 
-        public void PrintItems(Dictionary<int, Item> items)
+
+        public static void PrintItems(Dictionary<int, Item> items)
         {
             foreach (var item in items)
             {
@@ -31,7 +28,7 @@ namespace Online_Store
             }
         }
         //here is a method with the same name -> overload -> Polymorphism
-        public void PrintItems(List<Item> items)
+        public static void PrintItems(List<Item> items)
         {
             int i = 1;
             foreach (var item in items)
--- Simple-Online-Store/Simple-Online-Store/Checkout.cs
+++ Simple-Online-Store/Simple-Online-Store/Checkout.cs
@@ -7,13 +7,13 @@ namespace Online_Store
 {
     class Checkout
     {
-        public decimal CalculateTotalCost(List<Item> items)
+        public static decimal CalculateTotalCost(List<Item> items)
         {
             decimal totalCost = items.Sum(x => x.Price);
             return totalCost;
         }
         //example of overload
-        public decimal CalculateTotalCost(List<Item> items, int discount)
+        public static decimal CalculateTotalCost(List<Item> items, int discount)
         {
             decimal totalCost = items.Sum(x => x.Price) - items.Sum(x => x.Price) * discount / 100;
             return totalCost;
